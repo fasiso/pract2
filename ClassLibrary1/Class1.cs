61fd33a6830b3b86b1e443c2cb0b2071d35e1717
исп-34 <-34@LAB21-05>
2024-10-30 12:17:53 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+     public static class SumCalculator
+    {
+        public static int CalculateSum(string[] inputNumbers, int numberOfNumbers)
+        {
+            if (inputNumbers.Length != numberOfNumbers)
+            {
+                throw new ArgumentException("Неверное количество чисел.");
+            }
+
+            if (inputNumbers.Length == 0)
+            {
+                throw new ArgumentException("Необходимо ввести хотя бы одно число.");
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                int number;
+                if (!int.TryParse(inputNumbers[i], out number))
+                {
+                    throw new ArgumentException("Некорректно введено число.");
+                }
+
+                if (number < 8)
+                {
+                    sum += number;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+        
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using ClassLibrary1;
+
+namespace SumCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSum_Click(object sender, RoutedEventArgs e)
+        {
+            int numberOfNumbers;
+            if (!int.TryParse(NumberOfNumbers.Text, out numberOfNumbers))
+            {
+                MessageBox.Show("Некорректно введено количество чисел. Введите целое число.");
+                return;
+            }
+
+            if (numberOfNumbers < 1)
+            {
+                MessageBox.Show("Количество чисел должно быть не меньше 1.");
+                return;
+            }
+
+            string[] inputNumbers = InputNumbers.Text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            if (inputNumbers.Length != numberOfNumbers)
+            {
+                MessageBox.Show("Введено неверное количество чисел. Введите " + numberOfNumbers + " чисел.");
+                return;
+            }
+
+            if (inputNumbers.Length == 0)
+            {
+                MessageBox.Show("Необходимо ввести хотя бы одно число.");
+                return;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                int number;
+                if (!int.TryParse(inputNumbers[i], out number))
+                {
+                    MessageBox.Show("Некорректно введено число. Введите целые числа, разделенные пробелами.");
+                    return;
+                }
+
+                if (number < 8)
+                {
+                    sum += number;
+                }
+            }
+            Result.Text = sum.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Автор: Цыпленков Кирилл" + "\n" + "Группа ИСП-34" + "\n" + "Ввести n целых чисел. Найти сумму чисел < 8.");
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
